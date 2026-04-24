f52eedf9e5e012c6f62a5a41d6cdee1fa489a1a6
clawdtesting <clawdtesting@gmail.com>
2026-04-24 19:04:40 -0400

    Delete F0X directory

--- F0X/dashboard/src/components/Layout.tsx
+++
@@ -1,5 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-export function Layout({ children }: PropsWithChildren) {
-  return <div className="min-h-screen bg-slate-950 text-slate-100">{children}</div>;
-}
--- F0X/orchestratorchatalphav0/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-// In src/global.d.ts
-import { ExternalProvider } from 'web3-core';
-
-declare global {
-  interface Window {
-    ethereum: ExternalProvider;
-  }
-}
-
